0cdcd6cf66b810308bcb9618647dc792a61817ec
张锦江 <33757152@qq.com>
2018-05-08 17:18:52 +0800

    Initial Commit

---
+++ ZJJChosePhonePhotosBrowse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZJJChosePhonePhotosBrowse
+//
+//  Created by 张锦江 on 2018/5/8.
+//  Copyright © 2018年 xtayqria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
